871229527bbd6d35b24cfb27e7e08f12263eeaa5
csswee <157263616+csswee@users.noreply.github.com>
2025-05-24 16:29:22 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1617,7 +1617,7 @@ function App() {
           >
             <Paper
               component='a'
-              href='https://github.com/zqq-nuli/Navihive'
+              href='https://226626.xyz'
               target='_blank'
               rel='noopener noreferrer'
               elevation={2}
